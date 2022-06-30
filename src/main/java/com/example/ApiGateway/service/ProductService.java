012c4e01c3fa543ca2f36b5c9ce8c1b6235f35f9
maxim.irmscher <maxim.irmscher@idealo.de>
2022-06-30 09:29:01 +0200

    KK-67 implement rabbitmq for ProductComponents

--- src/main/java/com/example/ApiGateway/service/ProductService.java
+++ src/main/java/com/example/ApiGateway/service/ProductService.java
@@ -5,6 +5,7 @@ import com.example.ApiGateway.api.dto.ProductComponent;
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.core.DirectExchange;
 import org.springframework.amqp.core.Message;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
@@ -13,9 +14,11 @@ import org.springframework.beans.factory.annotation.Value;
 
 import java.lang.reflect.Type;
 import java.nio.charset.StandardCharsets;
+import java.util.Collections;
 import java.util.List;
 
 @RequiredArgsConstructor
+@Slf4j
 public class ProductService {
 
     @Autowired
@@ -34,9 +37,12 @@ public class ProductService {
                 routingKeyProductService,
                 new Message("getProductComponent".getBytes())
         );
-        Type founderListType = new TypeToken<List<ProductComponent>>(){}.getType();
+        if (m == null) {
+            log.error("error while receiving productComponents from ProductService");
+            return Collections.emptyList();
+        }
         String json = new String(m.getBody(), StandardCharsets.UTF_8);
-        return new Gson().fromJson(json, founderListType);
+        return new Gson().fromJson(json, new TypeToken<List<ProductComponent>>(){}.getType());
     }
 
 
