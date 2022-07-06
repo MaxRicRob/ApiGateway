cae44fadd83d70c7bb26c28dc65c3e32e8ab93a4
maxim.irmscher <maxim.irmscher@idealo.de>
2022-07-06 10:26:05 +0200

    ApiServiceTest

---
+++ src/test/java/com/example/ApiGateway/domain/ApiServiceTest.java
@@ -0,0 +1,88 @@
+package com.example.ApiGateway.domain;
+
+import com.example.ApiGateway.entity.CurrencyRequest;
+import com.example.ApiGateway.entity.PriceRequest;
+import com.example.ApiGateway.entity.Product;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.mockito.Mockito.verify;
+
+@ExtendWith(MockitoExtension.class)
+class ApiServiceTest {
+
+    @InjectMocks
+    ApiService apiService;
+
+    @Mock
+    ProductService productService;
+    @Mock
+    PriceService priceService;
+    @Mock
+    CurrencyService currencyService;
+    @Mock
+    Product product;
+    @Mock
+    PriceRequest priceRequest;
+    @Mock
+    CurrencyRequest currencyRequest;
+
+    @Test
+    void getDefaultProducts() {
+        apiService.getDefaultProducts();
+
+        verify(productService).getDefaultProducts();
+    }
+
+    @Test
+    void getProductComponents() {
+        apiService.getProductComponents();
+
+        verify(productService).getAllComponents();
+    }
+
+    @Test
+    void getProductsFromUser() {
+        apiService.getProductsFromUser("userName");
+
+        verify(productService).getProductsFromUser("userName");
+    }
+
+    @Test
+    void deleteProduct() {
+        apiService.deleteProduct("1");
+
+        verify(productService).deleteProduct("1");
+    }
+
+    @Test
+    void createProduct() {
+        apiService.createProduct(product);
+
+        verify(productService).createProduct(product);
+    }
+
+    @Test
+    void updateProduct() {
+        apiService.updateProduct(product);
+
+        verify(productService).updateProduct(product);
+    }
+
+    @Test
+    void getPrice() {
+        apiService.getPrice(priceRequest);
+
+        verify(priceService).getPrice(priceRequest);
+    }
+
+    @Test
+    void getCurrency() {
+        apiService.getCurrency(currencyRequest);
+
+        verify(currencyService).getCurrency(currencyRequest);
+    }
+}
