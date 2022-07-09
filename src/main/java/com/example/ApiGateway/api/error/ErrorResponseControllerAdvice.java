b228ddddc0f7109cc501f5f057fd80298509905b
maxim.irmscher <maxim.irmscher@idealo.de>
2022-07-09 19:47:43 +0200

    error response

--- src/main/java/com/example/ApiGateway/api/error/ErrorResponseControllerAdvice.java
+++ src/main/java/com/example/ApiGateway/api/error/ErrorResponseControllerAdvice.java
@@ -3,6 +3,7 @@ package com.example.ApiGateway.api.error;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import static org.springframework.http.HttpStatus.*;
@@ -12,6 +13,7 @@ import static org.springframework.http.HttpStatus.*;
 public class ErrorResponseControllerAdvice {
 
     @ExceptionHandler(ErrorResponseException.class)
+    @ResponseStatus(value = BAD_REQUEST, reason = "couldn't process request")
     public ResponseEntity<String> handleErrorResponseException(final ErrorResponseException exception) {
 
         return ResponseEntity
