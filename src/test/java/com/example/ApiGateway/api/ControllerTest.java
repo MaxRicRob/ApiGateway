585077be7b6d0c33aeb12879c6f7335d7aa348df
maxim.irmscher <maxim.irmscher@idealo.de>
2022-07-13 15:50:25 +0200

    add unittests

---
+++ src/test/java/com/example/ApiGateway/api/ControllerTest.java
@@ -0,0 +1,99 @@
+package com.example.ApiGateway.api;
+
+import com.example.ApiGateway.domain.ApiService;
+import com.example.ApiGateway.domain.entity.CurrencyRequest;
+import com.example.ApiGateway.domain.entity.PriceRequest;
+import com.example.ApiGateway.domain.entity.Product;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.UUID;
+
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.verify;
+
+
+@ExtendWith(MockitoExtension.class)
+class ControllerTest {
+
+    @InjectMocks
+    Controller controller;
+    @Mock
+    ApiService apiService;
+    @Mock
+    Product product;
+    @Mock
+    PriceRequest priceRequest;
+    @Mock
+    CurrencyRequest currencyRequest;
+
+    @Test
+    void getDefaultProducts() {
+
+        controller.getDefaultProducts();
+
+        verify(apiService).getDefaultProducts();
+    }
+
+    @Test
+    void getProductComponents() {
+
+        controller.getProductComponents();
+
+        verify(apiService).getProductComponents();
+    }
+
+    @Test
+    void getProductsFromUser() {
+
+        controller.getProductsFromUser("test");
+
+        verify(apiService).getProductsFromUser(eq("test"));
+    }
+
+    @Test
+    void deleteProduct() {
+        var uuid = UUID.randomUUID();
+
+        controller.deleteProduct(uuid.toString());
+
+        verify(apiService).deleteProduct(eq(uuid.toString()));
+    }
+
+    @Test
+    void createProduct() {
+
+        controller.createProduct(product);
+
+        verify(apiService).createProduct(product);
+
+    }
+
+    @Test
+    void updateProduct() {
+
+        controller.updateProduct(product);
+
+        verify(apiService).updateProduct(product);
+    }
+
+    @Test
+    void getPrice() {
+
+        controller.getPrice(priceRequest);
+
+        verify(apiService).getFromPriceService(priceRequest);
+
+    }
+
+    @Test
+    void getCurrency() {
+
+        controller.getCurrency(currencyRequest);
+
+        verify(apiService).getFromCurrencyService(currencyRequest);
+    }
+}
