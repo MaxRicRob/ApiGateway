f0464212079a84e60e90aac13916679eb49a08a8
maxim.irmscher <maxim.irmscher@idealo.de>
2022-07-14 16:38:30 +0200

    add ITTests

--- src/test/java/com/example/ApiGateway/api/ControllerTestIT.java
+++ src/test/java/com/example/ApiGateway/api/ControllerTestIT.java
@@ -2,6 +2,7 @@ package com.example.ApiGateway.api;
 
 import com.example.ApiGateway.domain.ApiService;
 import com.example.ApiGateway.domain.entity.DefaultProduct;
+import com.example.ApiGateway.domain.entity.Product;
 import com.example.ApiGateway.domain.entity.ProductComponent;
 import com.example.ApiGateway.error.ErrorResponseException;
 import com.fasterxml.jackson.core.type.TypeReference;
@@ -15,11 +16,14 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.List;
+import java.util.UUID;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.fail;
+import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -116,6 +120,84 @@ public class ControllerTestIT {
             fail();
         }
     }
+
+    @Test
+    void get_products_from_user_status_ok() {
+
+        try {
+            List<Product> products = List.of(
+                    new Product().setName("testProduct1"),
+                    new Product().setName("testProduct2")
+            );
+            when(apiService.getProductsFromUser("testUser")).thenReturn(products);
+
+            var mockMvcResult = mockMvc.perform(get("/products/testUser"))
+                    .andExpect(status().isOk())
+                    .andReturn();
+
+            List<Product> actualResults =
+                    objectMapper.readValue(mockMvcResult.getResponse().getContentAsString(), new TypeReference<>(){});
+            assertThat(actualResults.size()).isEqualTo(2);
+            assertThat(actualResults.get(0).getName()).isEqualTo("testProduct1");
+            assertThat(actualResults.get(1).getName()).isEqualTo("testProduct2");
+            verify(apiService).getProductsFromUser(eq("testUser"));
+
+        } catch (Exception e) {
+            fail();
+        }
+    }
+
+    @Test
+    void get_products_from_user_status_bad_request() {
+
+        try {
+            when(apiService.getProductsFromUser("testUser")).thenThrow(ErrorResponseException.class);
+            mockMvc.perform(get("/products/testUser"))
+                    .andExpect(status().isBadRequest());
+
+            verify(apiService).getProductsFromUser(eq("testUser"));
+
+        } catch (Exception e) {
+            fail();
+        }
+    }
+
+    @Test
+    void delete_status_ok() {
+
+        try {
+            var id = UUID.randomUUID().toString();
+            when(apiService.deleteProduct(id)).thenReturn("deleted");
+
+            var mockMvcResult = mockMvc.perform(delete("/products/"+id))
+                    .andExpect(status().isOk())
+                    .andReturn();
+
+            String actualResult = mockMvcResult.getResponse().getContentAsString();
+            assertThat(actualResult).isEqualTo("deleted");
+            verify(apiService).deleteProduct(eq(id));
+
+        } catch (Exception e) {
+            fail();
+        }
+    }
+
+    @Test
+    void delete_status_bad_request() {
+
+        try {
+            var id = UUID.randomUUID().toString();
+            when(apiService.deleteProduct(id)).thenThrow(ErrorResponseException.class);
+            mockMvc.perform(delete("/products/"+id))
+                    .andExpect(status().isBadRequest());
+
+            verify(apiService).deleteProduct(eq(id));
+        } catch (Exception e) {
+            fail();
+        }
+    }
+
+
 
 
 }
