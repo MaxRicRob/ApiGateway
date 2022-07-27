b136c839aab6a3230585a761b56c21a131750e97
maxim.irmscher <maxim.irmscher@idealo.de>
2022-07-27 10:03:14 +0200

    fix

--- src/main/java/com/example/ApiGateway/api/Controller.java
+++ src/main/java/com/example/ApiGateway/api/Controller.java
@@ -23,7 +23,7 @@ import static org.springframework.http.HttpStatus.CREATED;
 import static org.springframework.http.HttpStatus.OK;
 import static org.springframework.http.ResponseEntity.status;
 
-@CrossOrigin(origins = "http://localhost:3000/*")
+@CrossOrigin
 @Slf4j
 @RequiredArgsConstructor
 @RestController
